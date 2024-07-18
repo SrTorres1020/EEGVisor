77257c15babb48c0072541011fbf0ecd72962ac9
Juan Esteban Torres <srtorres1020@gmail.com>
2024-07-17 21:44:10 -0500

    Optimización de extraccion de datos .mat

--- BasedeDatos/Datos.mat/ExtraccionDeDatos.mat.py
+++ BasedeDatos/Datos.mat/ExtraccionDeDatos.mat.py
@@ -8,12 +8,8 @@ import numpy as np
 import pandas as pd
 from pymatreader import read_mat
 
-data = read_mat('507_Depression_REST.mat')
-rawdata = data['EEG']['data']
+data = read_mat('C:\\Users\\Fausto\\Documents\\github\\Archivos\\EEG_Depression\\507_Depression_REST.mat')
+df = pd.DataFrame(np.transpose(data['EEG']['data']))
 
-rawdatatranspuesta = np.transpose(rawdata)
-
-df = pd.DataFrame(rawdatatranspuesta)
-
-#Genera un archivo .csv
-df.to_csv('raw_data.csv', index=False)
+# #Genera un archivo .csv
+df.to_csv('C:\\Users\\Fausto\\Documents\\github\\Archivos\\EEG_Depression\\raw_data.csv', index=False)
